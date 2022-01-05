e7b1b0dbec0acc92b8a6c81b59f397cf25dcae35
ViktorEdman <viktor@edman.se>
2022-01-05 11:57:42 +0100

    componentized

--- part1/course-info/src/App.js
+++ part1/course-info/src/App.js
@@ -1,5 +1,17 @@
 import React from 'react'
 
+const Header = ({ course }) => <h1>{course}</h1>
+const Part = ({part, excercises}) => <p>{part} {excercises}</p>
+const Content = (props) => {
+  return (
+  <>
+    <Part part={props.part1} excercises={props.exercises1}/>
+    <Part part={props.part2} excercises={props.exercises2}/>
+    <Part part={props.part3} excercises={props.exercises3}/>
+  </>)
+}
+const Total = ({ total }) => <p>Number of exercises {total}</p>
+
 const App = () => {
   const course = 'Half Stack application development'
   const part1 = 'Fundamentals of React'
@@ -11,17 +23,15 @@ const App = () => {
 
   return (
     <div>
-      <h1>{course}</h1>
-      <p>
-        {part1} {exercises1}
-      </p>
-      <p>
-        {part2} {exercises2}
-      </p>
-      <p>
-        {part3} {exercises3}
-      </p>
-      <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+      <Header course={course} />
+      <Content
+        part1={part1}
+        exercises1={exercises1}
+        part2={part2}
+        exercises2={exercises2}
+        part3={part3}
+        exercises3={exercises3} />
+      <Total total={exercises1 + exercises2 + exercises3} />
     </div>
   )
 }
