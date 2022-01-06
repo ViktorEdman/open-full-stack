8254ebd813c3009cd4791a01fb77182a97163d36
ViktorEdman <viktor@edman.se>
2022-01-06 21:46:19 +0100

    implemented person filter search

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -2,18 +2,31 @@ import React, { useState } from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
+    {name: 'Arto Hellas', number: "040-1234567"},
+    {name: 'Viktor Edman', number: '0722-077793'},
+    {name: 'Eva Edman', number: '0739-131840'}
   ]) 
   const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
+  const [filter, setFilter] = useState('')
   
   const handleNameChange = (event) => {
     setNewName(event.target.value)
   }
 
+  const handleNumberChange = (event) => {
+    setNewNumber(event.target.value)
+  }
+
+  const handleFilterChange = (event) => {
+    setFilter(event.target.value)
+  }
+
   const addPerson = (event) => {
     event.preventDefault()
     const personObject = {
-      name: newName
+      name: newName,
+      number: newNumber
     }
     
     if (persons.filter(person => person.name === newName).length > 0) {
@@ -23,11 +36,22 @@ const App = () => {
     }
     setPersons(persons.concat(personObject))
     setNewName('')
+    setNewNumber('')
   }
+
+  const filteredPersons = filter 
+        ? persons.filter(person => person.name.toLowerCase().includes(filter.toLowerCase()))
+        : persons
 
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>
+        filter shown with <input 
+          type="text"
+          onChange={handleFilterChange} />
+      </div>
+      <h2>Add new person</h2>
       <form onSubmit={addPerson}>
         <div>
           name: <input 
@@ -35,13 +59,19 @@ const App = () => {
           onChange={handleNameChange}/>
         </div>
         <div>
+          number: <input
+          value={newNumber}
+          onChange={handleNumberChange}
+          />
+        </div>
+        <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
       <ul>
-        {persons.map(person => 
-        <li key={person.name}>{person.name}</li>)}
+        {filteredPersons.map(person =>
+          <li key={person.name+" "+person.number}>{person.name} {person.number}</li>) }
       </ul>
     </div>
   )
