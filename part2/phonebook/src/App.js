99c7c6dbc7f836843e4dfb8970715a08877057cf
ViktorEdman <viktor@edman.se>
2022-01-11 22:53:20 +0100

    added json-server to phonebook

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,18 +1,22 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 import Filter from './components/Filter'
 import PersonForm from './components/PersonForm'
 import Persons from './components/Persons'
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    {name: 'Arto Hellas', number: "040-1234567"},
-    {name: 'Viktor Edman', number: '0722-077793'},
-    {name: 'Eva Edman', number: '0739-131840'},
-    {name: 'Ada Lovelace', number: '031-714288'}
-  ]) 
+  const [persons, setPersons] = useState([]) 
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [filter, setFilter] = useState('')
+
+  useEffect(() => {
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => {
+        setPersons(response.data)
+      })
+  }, [])
   
   const handleNameChange = (event) => {
     setNewName(event.target.value)
